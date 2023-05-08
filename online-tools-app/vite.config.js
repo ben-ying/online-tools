8937c74688d1ffc9c92dc535f071f6aa5c12f5bd
benying1988@gmail.com <ben>
2023-05-08 17:38:11 +0800

    feat: build to public folder

--- online-tools-app/vite.config.js
+++ online-tools-app/vite.config.js
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { splitVendorChunkPlugin } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), splitVendorChunkPlugin()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
